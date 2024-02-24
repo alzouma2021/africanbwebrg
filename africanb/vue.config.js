15c89a44cf76632f49ff55638aa9b9814fcb464e
alzouma2021 <86126887+alzouma2021@users.noreply.github.com>
2024-02-24 18:22:14 +0000

    Update vue.config.js

--- africanb/vue.config.js
+++ africanb/vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    public: process.env.HEROKU_APP_NAME
+      ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`
+      : 'http://localhost:8080', // Remplacez localhost:8080 par le port de développement de votre choix
+    disableHostCheck: true,
+  },
   transpileDependencies: [
     'vuetify'
   ],
